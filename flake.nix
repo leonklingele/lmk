05fcefe81c48aaa1cd1ffab3f02990f9e572e9e1
Leon Klingele <git@leonklingele.de>
2025-07-08 13:06:24 +0200

    chore: nix flake update

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
